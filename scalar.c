84c22a1915bfa319aa1db59169ff9e7739eabaee
OscarLGH <OscarLGH@live.com>
2021-07-23 17:57:55 +0800

    TestBench:
    add SNaN testcase.
    add cr, vscr, fpscr output.
    add index to output for a clear diff.

--- scalar.c
+++ scalar.c
@@ -27,8 +27,8 @@ uint64_t __attribute__((aligned(16))) vector_array_src1[] = {
 	0x0001020304050607, 0x08090a0b0c0d0e0f,  /* integer normal, float normal */
 	0x0,0x0,				 /* integer 0, float 0 */
 	0x7f7f7f7f7f7f7f7f, 0x7f7f7f7f7f7f7f7f,  /* integer +big, float +normal */
-	0xffffffffffffffff, 0xffffffffffffffff,  /* integer -1, float NaN */
-	0xffffffffffffffff, 0xffffffffffffffff,  /* integer -1, float NaN */
+	0xffffffffffffffff, 0xffffffffffffffff,  /* integer -1, float QNaN, double QNaN*/
+	0xffffffffffffffff, 0xffffffffffffffff,  /* integer -1, float QNaN, double QNaN*/
 	0x0,0x0,				 /* integer 0, float 0 */
 	0x8000000080000000, 0x8000000080000000,  /* integer -Max, float -0 */
 	0x8000000000000000, 0x8000000000000000,  /* integer -Max, double -0 */
@@ -45,7 +45,8 @@ uint64_t __attribute__((aligned(16))) vector_array_src1[] = {
 	0x7f7fffffffff7f7f, 0xffffffffffffffff,  /* integer +a, double +MAX */
 	0x0080000000000001, 0x0080000000000001,  /* integer +a, double +MIN */
 	0xff7fffffffffff7f, 0xffffffffffffffff,  /* integer -a, double -MAX */
-	0x8080000000000001, 0x8080000000000001,  /* integer -a, double -MIN */
+	0xffbfffffffffffff, 0x7f7fffffffff7f7f,  /* integer -a, double SNaN */
+	0x7f7fffffffff7f7f, 0xffbfffffffffffff,  /* integer -a, double SNaN */
 };
 uint64_t __attribute__((aligned(16))) vector_array_src2[] = {
 	0x0001020304050607, 0x08090a0b0c0d0e0f,  /* integer normal, float normal */
@@ -70,6 +71,8 @@ uint64_t __attribute__((aligned(16))) vector_array_src2[] = {
 	0xff7fffffffffff7f, 0xffffffffffffffff,  /* integer -a, double -MAX */
 	0x0080000000000001, 0x0080000000000001,  /* integer +a, double +MIN */
 	0x7f7fffffffff7f7f, 0xffffffffffffffff,  /* integer +a, double +MAX */
+	0x7f7fffffffff7f7f, 0xffbfffffffffffff,  /* integer -a, double SNaN */
+	0x7f7fffffffff7f7f, 0xffbfffffffffffff,  /* integer -a, double SNaN */
 };
 uint64_t __attribute__((aligned(16))) vector_array_src3[] = {
 	0x0001020304050607, 0x08090a0b0c0d0e0f,  /* integer normal, float normal */
@@ -94,9 +97,32 @@ uint64_t __attribute__((aligned(16))) vector_array_src3[] = {
 	0xff7fffffffffff7f, 0xffffffffffffffff,  /* integer -a, double -MAX */
 	0x0080000000000001, 0x0080000000000001,  /* integer +a, double +MIN */
 	0x7f7fffffffff7f7f, 0xffffffffffffffff,  /* integer +a, double +MAX */
+	0x7f7fffffffff7f7f, 0xffbfffffffffffff,  /* integer -a, double SNaN */
+	0xffbfffffffffffff, 0x7f7fffffffff7f7f,  /* integer -a, double SNaN */
 };
 uint64_t __attribute__((aligned(16))) vector_array_dst[2] = {0};
 
+void load_cr(uint64_t val)
+{
+	asm (
+		"mtcr %0 \n"
+		:
+		:"r"(val)
+		:
+	);
+}
+
+void load_vscr(__uint128_t val)
+{
+	asm (
+		"li %%r0, 0 \n"
+		"lvx %%v9, %%r0, %0 \n"
+		"mtvscr %%v9 \n"
+		:
+		:"r"(&val)
+		:"r0"
+	);
+}
 
 void load_scalar_src1(void *vector)
 {
@@ -166,18 +192,64 @@ void store_scalar_dst2(void *vector)
 	);
 }
 
+uint64_t store_cr(void)
+{
+	uint64_t cr_val;
+	asm (
+		"mfcr %0 \n"
+		:"=r"(cr_val)
+		:
+		:
+	);
+	return cr_val;
+}
+
+void store_vscr(__uint128_t *ptr)
+{
+	__uint128_t vscr_val;
+	asm (
+		"mfvscr %%v9 \n"
+		"li %%r0, 0 \n"
+		"stvx %%v9, %%r0, %0 \n"
+		:
+		:"r"(ptr)
+		:"r0"
+	);
+}
+
+uint64_t store_fpscr(void)
+{
+	uint64_t fpscr_val;
+	asm (
+		"mffs %%f9 \n"
+		"li %%r0, 0 \n"
+		"stfdx %%f9, %%r0, %0"
+		:
+		:"r"(&fpscr_val)
+		:
+	);
+	return fpscr_val;
+}
+
+uint64_t __attribute__((aligned(16))) vscr[2] = {0};
+
 void main()
 {
 	int i;
-	for (i = 0; i < ARRAY_SIZE(vector_array_src1); i++) {
-		//printf("loading vector:%llx %llx\n", vector_array_ld[0], vector_array_ld[1]);
+	uint64_t cr = 0, fpscr = 0;
+	for (i = 0; i < ARRAY_SIZE(vector_array_src1) / 2; i++) {
 		load_scalar_src1(&vector_array_src1[i * 2]);
 		load_scalar_src2(&vector_array_src2[i * 2]);
 		load_scalar_src3(&vector_array_src3[i * 2]);
+		load_cr(0x0);
+		load_vscr(0x0);
 		operations();
+		cr = store_cr();
+		store_vscr((__uint128_t *)&vscr[0]);
+		fpscr = store_fpscr();
 		store_scalar_dst1(&vector_array_dst[0]);
-		printf("####output####{%016llx %016llx}\n",((uint64_t *)&vector_array_dst[0])[0], ((uint64_t *)&vector_array_dst[0])[1]);
+		printf("####output1 %02d####{DST 0x%016llx%016llx CR:%llx VSCR:%llx FPSCR:%llx}\n", i, vector_array_dst[0], vector_array_dst[1], cr, vscr[0], fpscr);
 		store_scalar_dst2(&vector_array_dst[0]);
-		printf("####output####{%016llx %016llx}\n",((uint64_t *)&vector_array_dst[0])[0], ((uint64_t *)&vector_array_dst[0])[1]);
+		printf("####output2 %02d####{DST 0x%016llx%016llx CR:%llx VSCR:%llx FPSCR:%llx}\n", i, vector_array_dst[0], vector_array_dst[1], cr, vscr[0], fpscr);
 	}
 }
--- vector.c
+++ vector.c
@@ -27,8 +27,8 @@ uint64_t __attribute__((aligned(16))) vector_array_src1[] = {
 	0x0001020304050607, 0x08090a0b0c0d0e0f,  /* integer normal, float normal */
 	0x0,0x0,				 /* integer 0, float 0 */
 	0x7f7f7f7f7f7f7f7f, 0x7f7f7f7f7f7f7f7f,  /* integer +big, float +normal */
-	0xffffffffffffffff, 0xffffffffffffffff,  /* integer -1, float NaN */
-	0xffffffffffffffff, 0xffffffffffffffff,  /* integer -1, float NaN */
+	0xffffffffffffffff, 0xffffffffffffffff,  /* integer -1, float QNaN, double QNaN*/
+	0xffffffffffffffff, 0xffffffffffffffff,  /* integer -1, float QNaN, double QNaN*/
 	0x0,0x0,				 /* integer 0, float 0 */
 	0x8000000080000000, 0x8000000080000000,  /* integer -Max, float -0 */
 	0x8000000000000000, 0x8000000000000000,  /* integer -Max, double -0 */
@@ -45,7 +45,8 @@ uint64_t __attribute__((aligned(16))) vector_array_src1[] = {
 	0x7f7fffffffff7f7f, 0xffffffffffffffff,  /* integer +a, double +MAX */
 	0x0080000000000001, 0x0080000000000001,  /* integer +a, double +MIN */
 	0xff7fffffffffff7f, 0xffffffffffffffff,  /* integer -a, double -MAX */
-	0x8080000000000001, 0x8080000000000001,  /* integer -a, double -MIN */
+	0xffbfffffffffffff, 0x7f7fffffffff7f7f,  /* integer -a, double SNaN */
+	0x7f7fffffffff7f7f, 0xffbfffffffffffff,  /* integer -a, double SNaN */
 };
 uint64_t __attribute__((aligned(16))) vector_array_src2[] = {
 	0x0001020304050607, 0x08090a0b0c0d0e0f,  /* integer normal, float normal */
@@ -70,6 +71,8 @@ uint64_t __attribute__((aligned(16))) vector_array_src2[] = {
 	0xff7fffffffffff7f, 0xffffffffffffffff,  /* integer -a, double -MAX */
 	0x0080000000000001, 0x0080000000000001,  /* integer +a, double +MIN */
 	0x7f7fffffffff7f7f, 0xffffffffffffffff,  /* integer +a, double +MAX */
+	0x7f7fffffffff7f7f, 0xffbfffffffffffff,  /* integer -a, double SNaN */
+	0x7f7fffffffff7f7f, 0xffbfffffffffffff,  /* integer -a, double SNaN */
 };
 uint64_t __attribute__((aligned(16))) vector_array_src3[] = {
 	0x0001020304050607, 0x08090a0b0c0d0e0f,  /* integer normal, float normal */
@@ -94,11 +97,34 @@ uint64_t __attribute__((aligned(16))) vector_array_src3[] = {
 	0xff7fffffffffff7f, 0xffffffffffffffff,  /* integer -a, double -MAX */
 	0x0080000000000001, 0x0080000000000001,  /* integer +a, double +MIN */
 	0x7f7fffffffff7f7f, 0xffffffffffffffff,  /* integer +a, double +MAX */
+	0x7f7fffffffff7f7f, 0xffbfffffffffffff,  /* integer -a, double SNaN */
+	0xffbfffffffffffff, 0x7f7fffffffff7f7f,  /* integer -a, double SNaN */
 };
 uint64_t __attribute__((aligned(16))) vector_array_dst[2] = {0};
 
+void load_cr(uint64_t val)
+{
+	asm (
+		"mtcr %0 \n"
+		:
+		:"r"(val)
+		:
+	);
+}
 
-void load_vector_src1(void *vector)
+void load_vscr(__uint128_t val)
+{
+	asm (
+		"li %%r0, 0 \n"
+		"lvx %%v9, %%r0, %0 \n"
+		"mtvscr %%v9 \n"
+		:
+		:"r"(&val)
+		:"r0","v9"
+	);
+}
+
+void load_vector_src1(__uint128_t *vector)
 {
 	asm (
 		"li %%r0, 0 \n"
@@ -109,7 +135,7 @@ void load_vector_src1(void *vector)
 	);
 }
 
-void load_vector_src2(void *vector)
+void load_vector_src2(__uint128_t *vector)
 {
 	asm (
 		"li %%r0, 0 \n"
@@ -120,7 +146,7 @@ void load_vector_src2(void *vector)
 	);
 }
 
-void load_vector_src3(void *vector)
+void load_vector_src3(__uint128_t *vector)
 {
 	asm (
 		"li %%r0, 0 \n"
@@ -141,7 +167,7 @@ void operations()
 	);
 }
 
-void store_vector_dst(void *vector)
+void store_vector_dst(__uint128_t *vector)
 {
 	asm (
 		"li %%r0, 0 \n"
@@ -164,18 +190,51 @@ uint64_t store_cr(void)
 	return cr_val;
 }
 
+void store_vscr(__uint128_t *ptr)
+{
+	__uint128_t vscr_val;
+	asm (
+		"mfvscr %%v9 \n"
+		"li %%r0, 0 \n"
+		"stvx %%v9, %%r0, %0 \n"
+		:
+		:"r"(ptr)
+		:"r0","v9"
+	);
+}
+
+uint64_t store_fpscr(void)
+{
+	uint64_t fpscr_val;
+	asm (
+		"mffs %%f9 \n"
+		"li %%r0, 0 \n"
+		"stfdx %%f9, %%r0, %0"
+		:
+		:"r"(&fpscr_val)
+		:
+	);
+	return fpscr_val;
+}
+
+uint64_t __attribute__((aligned(16))) vscr[2] = {0};
+
 void main()
 {
 	int i;
-	uint64_t cr;
-	for (i = 0; i < ARRAY_SIZE(vector_array_src1); i++) {
-		//printf("loading vector:%llx %llx\n", vector_array_ld[0], vector_array_ld[1]);
+	uint64_t cr = 0, fpscr = 0;
+	
+	for (i = 0; i < ARRAY_SIZE(vector_array_src1) / 2; i++) {
 		load_vector_src1(&vector_array_src1[i * 2]);
 		load_vector_src2(&vector_array_src2[i * 2]);
 		load_vector_src3(&vector_array_src3[i * 2]);
+		load_cr(0x0);
+		load_vscr(0x0);
 		operations();
 		cr = store_cr();
+		store_vscr((__uint128_t *)&vscr[0]);
+		fpscr = store_fpscr();
 		store_vector_dst(&vector_array_dst[0]);
-		printf("####output####{%016llx %016llx}\n",((uint64_t *)&vector_array_dst[0])[0], ((uint64_t *)&vector_array_dst[0])[1]);
+		printf("####output %02d####{DST 0x%016llx%016llx CR:%llx VSCR:%llx FPSCR:%llx}\n", i, vector_array_dst[0], vector_array_dst[1], cr, vscr[0], fpscr);
 	}
 }
